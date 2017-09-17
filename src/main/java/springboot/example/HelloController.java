786f223886a03748cd189d166f08faa197867fbc
wangkuan1987 <15882695440@163.com>
2017-09-17 18:29:10 +0800

    20170917

---
+++ src/main/java/springboot/example/HelloController.java
@@ -0,0 +1,23 @@
+package springboot.example;
+
+import com.example.demo.DemoApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+}
